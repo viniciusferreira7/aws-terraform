66c6c355f921397c19b1906a9a6c3b033be75c61
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-29 10:06:52 -0300

    feat: add s3 to use on backend

--- main.tf
+++ main.tf
@@ -4,9 +4,43 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+
+  }
+  backend "s3" {
+    bucket = "aws-terraform-s3"
+    key    = "state/terraform.tfstate"
+    region = var.region
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = var.region
+  profile = var.profile
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "aws-terraform"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+  type        = string
+  default     = ""
+  description = "AWS profile configured in CLI"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
